f9af55699c88174dee4d178884cc04e440387358
sanjay babu <32395477+san3ncrypt3d@users.noreply.github.com>
2021-04-11 11:19:22 -0400

    Create scanner.sh

---
+++ scanner.sh
@@ -0,0 +1,28 @@
+#/!bin/bash
+
+domain="$1"
+
+timestamp=$(date '+%s');
+
+if [ -z "$domain" ]; then
+    echo -e "Enter a valid URL.\r"
+    exit 1
+fi
+
+/opt/zaproxy/zap.sh -quickurl "$domain" -newsession "$timestamp" -cmd;
+
+
+
+Title="DAST Report"
+By="san3ncrypt3d"
+For="xx"
+Scan_Date=$(date -d @$timestamp)
+Report_Date=$(date -d @$timestamp)
+Scan_Ver="1"
+Report_Ver="1"
+Description="DAST Automated scan by ZAP"
+file="$timestamp"
+
+/opt/zaproxy/zap.sh -export_report "$HOME"/"$file".xhtml -source_info "$Title;$By;$For;$Scan_Date;$Report_Date;$Scan_Ver;$Report_Ver;$Description" -alert_severity "t;t;t;t" -alert_details "t;t;t;t;t;t;t;t;t;t" -session "$timestamp.session" -cmd
+
+wkhtmltopdf "$HOME"/"$file".xhtml "$HOME"/"$file".pdf

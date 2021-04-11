e59ae04651e4b7512e742e6ba10b2806369febb7
sanjay babu <32395477+san3ncrypt3d@users.noreply.github.com>
2021-04-11 12:25:47 -0400

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -21,8 +21,10 @@ Report_Date=$(date -d @$timestamp)
 Scan_Ver="1"
 Report_Ver="1"
 Description="DAST Automated scan by ZAP"
-file="$timestamp"
+file_tmp=$(echo "$1" | awk -F/ '{print $3}' | awk -F. '{print $1}')
+file="$file_tmp$timestamp"
 
 /opt/zaproxy/zap.sh -export_report "$HOME"/"$file".xhtml -source_info "$Title;$By;$For;$Scan_Date;$Report_Date;$Scan_Ver;$Report_Ver;$Description" -alert_severity "t;t;t;t" -alert_details "t;t;t;t;t;t;t;t;t;t" -session "$timestamp.session" -cmd
 
 wkhtmltopdf "$HOME"/"$file".xhtml "$HOME"/"$file".pdf
+pandoc -o "$HOME"/"$file".docx "$HOME"/"$file".xhtml
